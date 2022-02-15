428b55760e8ca74a21be64f6f8226deed0a79bb6
George, B. Smith <georgebsmith.tech@gmail.com>
2022-02-15 08:18:32 +0100

    fixed the banner

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,16 +1,22 @@
 export default function Welcome() {
-    
-    return (        
-        <div className='welcomediv'>           
-            <div className='welcome'>
-                <h2>Be Found Online!</h2>
-                <h2>Its simple!</h2>
-                <p className='inter bold500 text-white'>With our expertise, we fuel your social media and create apps that promote engaging experience for your business.</p>
-            </div>
-            <div className='welcomimg '>
-                <img src="images/HomePage/Brandr hero 01 1 (1).png" alt=""/>
-            </div>
-        </div>
-    )
+  return (
+    <div className="welcomediv">
+      <div className="welcome">
+        <h2>Be Found Online!</h2>
+        <h2>Its simple!</h2>
+        <p className="inter bold500 text-white">
+          With our expertise, we fuel your social media and create apps that
+          promote engaging experience for your business.
+        </p>
+      </div>
+      <div className="welcomimg ">
+        <img
+          src="images/HomePage/Brandr hero 01 1 (1).png"
+          alt=""
+          style={{ width: "100%" }}
+        />
+      </div>
+    </div>
+  );
 }
 // flex f14 space-between
